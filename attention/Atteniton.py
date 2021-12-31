1db372a87786a41b0074f252cbd754f9b4f71272
yuta-0514 <y.akutsu0514@gmail.com>
2021-12-31 09:39:48 +0000

    Attention

--- attention/Atteniton.py
+++ attention/Atteniton.py
@@ -2,14 +2,19 @@ import numpy as np
 import torch
 from torch import nn
 from torch.nn import init
-from torch.nn.parameter import Parameter
 
-class BAMBlock(nn.Module):
+from ECA import ECAAttention #Channel Attention
+from EPSA import PSA #Channel Attention
+from SGE import SpatialGroupEnhance  #Spatial Attention
 
-    def __init__(self, channel=512,reduction=16):
+
+# BAMベース
+class Attention_1(nn.Module):
+
+    def __init__(self, channel):
         super().__init__()
-        self.ca=ChannelAttention(channel=channel,reduction=reduction)
-        self.sa=SpatialAttention(channel=channel,reduction=reduction)
+        self.ca=ECAAttention(kernel_size=3)
+        self.sa=SpatialGroupEnhance(groups=8)
         self.sigmoid=nn.Sigmoid()
 
     def forward(self, x):
@@ -20,13 +25,13 @@ class BAMBlock(nn.Module):
         out=(1+weight)*x
         return out
 
+# CBAMベース
+class Attention_2(nn.Module):
 
-class CBAMBlock(nn.Module):
-
-    def __init__(self, channel,reduction=16):
+    def __init__(self,channel):
         super().__init__()
-        self.ca=ChannelAttention(channel=channel,reduction=reduction)
-        self.sa=SpatialAttention()
+        self.ca=PSA(channel=channel)
+        self.sa=SpatialGroupEnhance(groups=8)
 
     def forward(self, x):
         b, c, _, _ = x.size()
@@ -36,19 +41,16 @@ class CBAMBlock(nn.Module):
         return out+residual
 
 
-class ShuffleAttention(nn.Module):
+from CBAM import ChannelAttention,SpatialAttention
+# shuffleベース
+class Attention_3(nn.Module):
 
     def __init__(self, channel,reduction=16,G=8):
         super().__init__()
         self.G=G
         self.channel=channel
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.gn = nn.GroupNorm(channel // (2 * G), channel // (2 * G))
-        self.cweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.cbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.sbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sigmoid=nn.Sigmoid()
+        self.ca=ChannelAttention(channel=channel//self.G,reduction=reduction)
+        self.sa=SpatialAttention()
 
     @staticmethod
     def channel_shuffle(x, groups):
@@ -63,76 +65,16 @@ class ShuffleAttention(nn.Module):
 
     def forward(self, x):
         b, c, h, w = x.size()
+
         #group into subfeatures
         x=x.view(b*self.G,-1,h,w) #bs*G,c//G,h,w
+        residual=x
 
-        #channel_split
-        x_0,x_1=x.chunk(2,dim=1) #bs*G,c//(2*G),h,w
-
-        #channel attention
-        x_channel=self.avg_pool(x_0) #bs*G,c//(2*G),1,1
-        x_channel=self.cweight*x_channel+self.cbias #bs*G,c//(2*G),1,1
-        x_channel=x_0*self.sigmoid(x_channel)
-
-        #spatial attention
-        x_spatial=self.gn(x_1) #bs*G,c//(2*G),h,w
-        x_spatial=self.sweight*x_spatial+self.sbias #bs*G,c//(2*G),h,w
-        x_spatial=x_1*self.sigmoid(x_spatial) #bs*G,c//(2*G),h,w
+        out=x*self.ca(x)
+        out=out*self.sa(out)
+        out=out+residual
 
         # concatenate along channel axis
-        out=torch.cat([x_channel,x_spatial],dim=1)  #bs*G,c//G,h,w
-        out=out.contiguous().view(b,-1,h,w)
-
-        # channel shuffle
-        out = self.channel_shuffle(out, 2)
-        return out
-
-
-class NewAttention(nn.Module):
-
-    def __init__(self, channel,reduction=16,G=8):
-        super().__init__()
-        self.G=G
-        self.channel=channel
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.gn = nn.GroupNorm(channel // (2 * G), channel // (2 * G))
-        self.cweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.cbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.sbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sigmoid=nn.Sigmoid()
-
-    @staticmethod
-    def channel_shuffle(x, groups):
-        b, c, h, w = x.shape
-        x = x.reshape(b, groups, -1, h, w)
-        x = x.permute(0, 2, 1, 3, 4)
-
-        # flatten
-        x = x.reshape(b, -1, h, w)
-
-        return x
-
-    def forward(self, x):
-        b, c, h, w = x.size()
-        #group into subfeatures
-        x=x.view(b*self.G,-1,h,w) #bs*G,c//G,h,w
-
-        #channel_split
-        x_0,x_1=x.chunk(2,dim=1) #bs*G,c//(2*G),h,w
-
-        #channel attention
-        x_channel=self.avg_pool(x_0) #bs*G,c//(2*G),1,1
-        x_channel=self.cweight*x_channel+self.cbias #bs*G,c//(2*G),1,1
-        x_channel=x_0*self.sigmoid(x_channel)
-
-        #spatial attention
-        x_spatial=self.gn(x_1) #bs*G,c//(2*G),h,w
-        x_spatial=self.sweight*x_spatial+self.sbias #bs*G,c//(2*G),h,w
-        x_spatial=x_1*self.sigmoid(x_spatial) #bs*G,c//(2*G),h,w
-
-        # concatenate along channel axis
-        out=torch.cat([x_channel,x_spatial],dim=1)  #bs*G,c//G,h,w
         out=out.contiguous().view(b,-1,h,w)
 
         # channel shuffle
@@ -141,7 +83,7 @@ class NewAttention(nn.Module):
 
 
 input=torch.randn(1,256,160,160)
-se = NewAttention(channel=256)
+se = Attention_3(channel=256)
 output=se(input)
 print(output.shape)
 
