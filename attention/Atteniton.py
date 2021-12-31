e8f0c39c5fcb1828b2cceca55e3881fe39330468
yuta-0514 <y.akutsu0514@gmail.com>
2021-12-31 12:06:08 +0000

    Attention

--- attention/Atteniton.py
+++ attention/Atteniton.py
@@ -40,17 +40,21 @@ class Attention_2(nn.Module):
         out=out*self.sa(out)
         return out+residual
 
-
-from CBAM import ChannelAttention,SpatialAttention
-# shuffleベース
-class Attention_3(nn.Module):
+# Shuffleベース
+from torch.nn.parameter import Parameter
+class ShuffleAttention(nn.Module):
 
     def __init__(self, channel,reduction=16,G=8):
         super().__init__()
         self.G=G
         self.channel=channel
-        self.ca=ChannelAttention(channel=channel//self.G,reduction=reduction)
-        self.sa=SpatialAttention()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.gn = nn.GroupNorm(channel // (2 * G), channel // (2 * G))
+        self.cweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
+        self.cbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
+        self.sweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
+        self.sbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
+        self.sigmoid=nn.Sigmoid()
 
     @staticmethod
     def channel_shuffle(x, groups):
@@ -65,16 +69,24 @@ class Attention_3(nn.Module):
 
     def forward(self, x):
         b, c, h, w = x.size()
-
         #group into subfeatures
         x=x.view(b*self.G,-1,h,w) #bs*G,c//G,h,w
-        residual=x
 
-        out=x*self.ca(x)
-        out=out*self.sa(out)
-        out=out+residual
+        #channel_split
+        x_0,x_1=x.chunk(2,dim=1) #bs*G,c//(2*G),h,w
+
+        #channel attention
+        x_channel=self.avg_pool(x_0) #bs*G,c//(2*G),1,1
+        x_channel=self.cweight*x_channel+self.cbias #bs*G,c//(2*G),1,1
+        x_channel=x_0*self.sigmoid(x_channel)
+
+        #spatial attention
+        x_spatial=self.gn(x_1) #bs*G,c//(2*G),h,w
+        x_spatial=self.sweight*x_spatial+self.sbias #bs*G,c//(2*G),h,w
+        x_spatial=x_1*self.sigmoid(x_spatial) #bs*G,c//(2*G),h,w
 
         # concatenate along channel axis
+        out=torch.cat([x_channel,x_spatial],dim=1)  #bs*G,c//G,h,w
         out=out.contiguous().view(b,-1,h,w)
 
         # channel shuffle
@@ -82,8 +94,56 @@ class Attention_3(nn.Module):
         return out
 
 
+# EPSAベース
+class PSA(nn.Module):
+
+    def __init__(self, channel,reduction=8,S=4):
+        super().__init__()
+        self.S=S
+
+        self.convs=[]
+        for i in range(S):
+            self.convs.append(nn.Conv2d(channel//S,channel//S,kernel_size=2*(i+1)+1,padding=i+1))
+
+        self.se_blocks=[]
+        for i in range(S):
+            self.se_blocks.append(nn.Sequential(
+                nn.AdaptiveAvgPool2d(1),
+                nn.Conv2d(channel//S, channel // (S*reduction),kernel_size=1, bias=False),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(channel // (S*reduction), channel//S,kernel_size=1, bias=False),
+                nn.Sigmoid()
+            ))
+        
+        self.softmax=nn.Softmax(dim=1)
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Step1:SPC module
+        SPC_out=x.view(b,self.S,c//self.S,h,w) #bs,s,ci,h,w
+        for idx,conv in enumerate(self.convs):
+            SPC_out[:,idx,:,:,:]=conv(SPC_out[:,idx,:,:,:])
+
+        #Step2:SE weight
+        se_out=[]
+        for idx,se in enumerate(self.se_blocks):
+            se_out.append(se(SPC_out[:,idx,:,:,:]))
+        SE_out=torch.stack(se_out,dim=1)
+        SE_out=SE_out.expand_as(SPC_out)
+
+        #Step3:Softmax
+        softmax_out=self.softmax(SE_out)
+
+        #Step4:SPA
+        PSA_out=SPC_out*softmax_out
+        PSA_out=PSA_out.view(b,-1,h,w)
+
+        return PSA_out
+
+
 input=torch.randn(1,256,160,160)
-se = Attention_3(channel=256)
+se = PSA(channel=256)
 output=se(input)
 print(output.shape)
 
