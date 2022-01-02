adf15afba62afb018983ef57f98ef49cb303a271
yuta-0514 <y.akutsu0514@gmail.com>
2022-01-02 11:58:51 +0000

    Shuffle Attention

--- attention/Shuffle_Attention.py
+++ attention/Shuffle_Attention.py
@@ -1,43 +1,26 @@
-import numpy as np
 import torch
-from torch import nn
-from torch.nn import init
+import torch.nn as nn
 from torch.nn.parameter import Parameter
 
 
 class ShuffleAttention(nn.Module):
 
-    def __init__(self, channel,reduction=16,G=8):
-        super().__init__()
-        self.G=G
-        self.channel=channel
+    def __init__(self, channel, groups=64):
+        super(ShuffleAttention, self).__init__()
+        self.groups = groups
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.gn = nn.GroupNorm(channel // (2 * G), channel // (2 * G))
-        self.cweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.cbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.sbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sigmoid=nn.Sigmoid()
+        self.cweight = Parameter(torch.zeros(1, channel // (2 * groups), 1, 1))
+        self.cbias = Parameter(torch.ones(1, channel // (2 * groups), 1, 1))
+        self.sweight = Parameter(torch.zeros(1, channel // (2 * groups), 1, 1))
+        self.sbias = Parameter(torch.ones(1, channel // (2 * groups), 1, 1))
 
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
+        self.sigmoid = nn.Sigmoid()
+        self.gn = nn.GroupNorm(channel // (2 * groups), channel // (2 * groups))
 
     @staticmethod
     def channel_shuffle(x, groups):
         b, c, h, w = x.shape
+
         x = x.reshape(b, groups, -1, h, w)
         x = x.permute(0, 2, 1, 3, 4)
 
@@ -47,35 +30,86 @@ class ShuffleAttention(nn.Module):
         return x
 
     def forward(self, x):
-        b, c, h, w = x.size()
-        #group into subfeatures
-        x=x.view(b*self.G,-1,h,w) #bs*G,c//G,h,w
+        b, c, h, w = x.shape
 
-        #channel_split
-        x_0,x_1=x.chunk(2,dim=1) #bs*G,c//(2*G),h,w
+        x = x.reshape(b * self.groups, -1, h, w)
+        x_0, x_1 = x.chunk(2, dim=1)
 
-        #channel attention
-        x_channel=self.avg_pool(x_0) #bs*G,c//(2*G),1,1
-        x_channel=self.cweight*x_channel+self.cbias #bs*G,c//(2*G),1,1
-        x_channel=x_0*self.sigmoid(x_channel)
+        # channel attention
+        xn = self.avg_pool(x_0)
+        xn = self.cweight * xn + self.cbias
+        xn = x_0 * self.sigmoid(xn)
 
-        #spatial attention
-        x_spatial=self.gn(x_1) #bs*G,c//(2*G),h,w
-        x_spatial=self.sweight*x_spatial+self.sbias #bs*G,c//(2*G),h,w
-        x_spatial=x_1*self.sigmoid(x_spatial) #bs*G,c//(2*G),h,w
+        # spatial attention
+        xs = self.gn(x_1)
+        xs = self.sweight * xs + self.sbias
+        xs = x_1 * self.sigmoid(xs)
 
         # concatenate along channel axis
-        out=torch.cat([x_channel,x_spatial],dim=1)  #bs*G,c//G,h,w
-        out=out.contiguous().view(b,-1,h,w)
+        out = torch.cat([xn, xs], dim=1)
+        out = out.reshape(b, -1, h, w)
 
-        # channel shuffle
         out = self.channel_shuffle(out, 2)
         return out
 
 
-input=torch.randn(1,256,160,160)
-se = ShuffleAttention(channel=256)
-output=se(input)
-print(output.shape)
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
 
- 
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class SABottleneck(nn.Module):
+    expansion = 4
+    __constants__ = ['downsample']
+
+    def __init__(self, channel, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(SABottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(channel//4 * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(channel, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, channel//4 * self.expansion)
+        self.bn3 = norm_layer(channel//4 * self.expansion)
+        self.sa = ShuffleAttention(channel//4 * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.sa(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+input=torch.randn(1,256,160,160)
+psa = SABottleneck(channel=256)
+output=psa(input)
+print(output.shape)
