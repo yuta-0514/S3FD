7bda46ec255f6392fb250346a041dc9389d1bdb6
yuta-0514 <y.akutsu0514@gmail.com>
2022-01-04 02:30:33 +0000

    Attention

--- attention/Attention.py
+++ attention/Attention.py
@@ -72,7 +72,7 @@ class PSA_ECA(nn.Module):
         return out
 
 
-# 2. PSA＋SGE
+# 2. PSA＋SGE -> △
 class SEWeightModule(nn.Module):
 
     def __init__(self, channels, reduction=16):
@@ -92,131 +92,10 @@ class SEWeightModule(nn.Module):
 
         return weight
 
-class PSA_SGE(nn.Module):
 
-    def __init__(self, channel, groups=4):
-        super(PSA_SGE, self).__init__()
-        self.se = SEWeightModule(channel // 4)
-        self.split_channel = channel // 4
-        self.softmax = nn.Softmax(dim=1)
+# 3. 3×3Conv -> EPSA
 
-        # SGE
-        self.groups=groups
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.weight=nn.Parameter(torch.zeros(1,groups,1,1))
-        self.bias=nn.Parameter(torch.zeros(1,groups,1,1))
-        self.sig=nn.Sigmoid()
-
-        self.cahnnel = channel//self.groups
-
-    def forward(self, x):
-        batch_size, c, h,w=x.shape
-        x=x.view(batch_size*self.groups,-1,h,w) #bs*g,dim//g,h,w
-        xn=x*self.avg_pool(x) #bs*g,dim//g,h,w
-        xn=xn.sum(dim=1,keepdim=True) #bs*g,1,h,w
-        t=xn.view(batch_size*self.groups,-1) #bs*g,h*w
-
-        t=t-t.mean(dim=1,keepdim=True) #bs*g,h*w
-        std=t.std(dim=1,keepdim=True)+1e-5
-        t=t/std #bs*g,h*w
-        t=t.view(batch_size,self.groups,h,w) #bs,g,h*w
-        
-        t=t*self.weight+self.bias #bs,g,h*w
-        t=t.view(batch_size*self.groups,1,h,w) #bs*g,1,h,w
-        x=x*self.sig(t)
-        x =x.view(batch_size,c,h,w)
-        feats = x.view(batch_size, 4, self.split_channel, x.shape[2], x.shape[3])
-
-        x1 = x[:,:self.cahnnel,:,:]
-        x2 = x[:,self.cahnnel:self.cahnnel*2,:,:]
-        x3 = x[:,self.cahnnel*2:self.cahnnel*3,:,:]
-        x4 = x[:,self.cahnnel*3:,:,:]
-
-        x1_se = self.se(x1)
-        x2_se = self.se(x2)
-        x3_se = self.se(x3)
-        x4_se = self.se(x4)
-
-
-        x_se = torch.cat((x1_se, x2_se, x3_se, x4_se), dim=1)
-        attention_vectors = x_se.view(batch_size, 4, self.split_channel, 1, 1)
-        attention_vectors = self.softmax(attention_vectors)
-        feats_weight = feats * attention_vectors
-        for i in range(4):
-            x_se_weight_fp = feats_weight[:, i, :, :]
-            if i == 0:
-                out = x_se_weight_fp
-            else:
-                out = torch.cat((x_se_weight_fp, out), 1)
-
-        return out
-
-
-# 3. PSA+SGE+ECA
-class PSA_SGE_ECA(nn.Module):
-
-    def __init__(self, channel, groups=4):
-        super(PSA_SGE_ECA, self).__init__()
-        self.split_channel = channel // 4
-        self.softmax = nn.Softmax(dim=1)
-
-        self.se = ECAWeight(kernel_size=3)
-
-        # SGE
-        self.groups=groups
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.weight=nn.Parameter(torch.zeros(1,groups,1,1))
-        self.bias=nn.Parameter(torch.zeros(1,groups,1,1))
-        self.sig=nn.Sigmoid()
-
-        self.cahnnel = channel//self.groups
-
-    def forward(self, x):
-        batch_size, c, h,w=x.shape
-        x=x.view(batch_size*self.groups,-1,h,w) #bs*g,dim//g,h,w
-        xn=x*self.avg_pool(x) #bs*g,dim//g,h,w
-        xn=xn.sum(dim=1,keepdim=True) #bs*g,1,h,w
-        t=xn.view(batch_size*self.groups,-1) #bs*g,h*w
-
-        t=t-t.mean(dim=1,keepdim=True) #bs*g,h*w
-        std=t.std(dim=1,keepdim=True)+1e-5
-        t=t/std #bs*g,h*w
-        t=t.view(batch_size,self.groups,h,w) #bs,g,h*w
-        
-        t=t*self.weight+self.bias #bs,g,h*w
-        t=t.view(batch_size*self.groups,1,h,w) #bs*g,1,h,w
-        x=x*self.sig(t)
-        x =x.view(batch_size,c,h,w)
-        feats = x.view(batch_size, 4, self.split_channel, x.shape[2], x.shape[3])
-
-        x1 = x[:,:self.cahnnel,:,:]
-        x2 = x[:,self.cahnnel:self.cahnnel*2,:,:]
-        x3 = x[:,self.cahnnel*2:self.cahnnel*3,:,:]
-        x4 = x[:,self.cahnnel*3:,:,:]
-
-        x1_se = self.se(x1)
-        x2_se = self.se(x2)
-        x3_se = self.se(x3)
-        x4_se = self.se(x4)
-
-
-        x_se = torch.cat((x1_se, x2_se, x3_se, x4_se), dim=1)
-        attention_vectors = x_se.view(batch_size, 4, self.split_channel, 1, 1)
-        attention_vectors = self.softmax(attention_vectors)
-        feats_weight = feats * attention_vectors
-        for i in range(4):
-            x_se_weight_fp = feats_weight[:, i, :, :]
-            if i == 0:
-                out = x_se_weight_fp
-            else:
-                out = torch.cat((x_se_weight_fp, out), 1)
-
-        return out
-
-
-# 4. 3×3Conv -> EPSA
-
-# 5. Suffle Attentionをそのまま利用する
+# 4. Suffle Attentionをそのまま利用する
 
 input=torch.randn(1,256,160,160)
 psa = PSA_ECA(channel=256)
