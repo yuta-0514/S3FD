42a002e277f24971326920c137691e0e1d802d26
yuta-0514 <y.akutsu0514@gmail.com>
2022-01-04 08:20:27 +0000

    SANet

--- attention/Shuffle_Attention.py
+++ attention/Shuffle_Attention.py
@@ -65,26 +65,18 @@ def conv1x1(in_planes, out_planes, stride=1):
 
 
 class SABlock(nn.Module):
-    expansion = 4
-    __constants__ = ['downsample']
 
-    def __init__(self, channel, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None):
+    def __init__(self, channel):
         super(SABlock, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        width = int(channel//4 * (base_width / 64.)) * groups
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(channel, width)
-        self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, channel//4 * self.expansion)
-        self.bn3 = norm_layer(channel//4 * self.expansion)
-        self.sa = ShuffleAttention(channel//4 * 4)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
+
+        self.conv1 = conv1x1(channel, channel//4)
+        self.bn1 = nn.BatchNorm2d(channel//4)
+        self.conv2 = conv3x3(channel//4, channel//4)
+        self.bn2 = nn.BatchNorm2d(channel//4)
+        self.conv3 = conv1x1(channel//4, channel)
+        self.bn3 = nn.BatchNorm2d(channel)
+        self.sa = ShuffleAttention(channel)
+        self.relu = nn.ReLU(inplace=False)
 
     def forward(self, x):
         identity = x
@@ -101,10 +93,7 @@ class SABlock(nn.Module):
         out = self.bn3(out)
         out = self.sa(out)
 
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
+        out = out + identity
         out = self.relu(out)
 
         return out
