59c183d9b171edd055020654e79c68c732972263
yuta-0514 <y.akutsu0514@gmail.com>
2022-01-27 10:29:26 +0000

    vgg16

--- vgg16.py
+++ vgg16.py
@@ -1,16 +1,15 @@
 import torch
-import torch.nn as nn
+import torch.nn as tnn
 from torchvision.datasets import CIFAR100
 import torchvision.transforms as transforms
 
-
-BATCH_SIZE = 4
+BATCH_SIZE = 10
 LEARNING_RATE = 0.01
-EPOCH = 300
-N_CLASSES = 25
+EPOCH = 50
+N_CLASSES = 100
 
 transform = transforms.Compose([
-    transforms.RandomResizedCrop(640),
+    transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(mean = [ 0.485, 0.456, 0.406 ],
@@ -18,90 +17,69 @@ transform = transforms.Compose([
     ])
 
 trainData = CIFAR100("CIFAR100", train=True, download=True, transform=transform)
-testData = CIFAR100("CIFAR100", train=False, download=True, transform=transform)
-
+testData = CIFAR100("CIFAR100", train=False, download=True, transform=transforms)
 trainLoader = torch.utils.data.DataLoader(dataset=trainData, batch_size=BATCH_SIZE, shuffle=True)
 testLoader = torch.utils.data.DataLoader(dataset=testData, batch_size=BATCH_SIZE, shuffle=False)
 
-
-
-def vgg_fc_layer(size_in, size_out):
-    layer = nn.Sequential(
-        nn.Linear(size_in, size_out),
-        nn.BatchNorm1d(size_out),
-        nn.ReLU()
+def conv_layer(chann_in, chann_out, k_size, p_size):
+    layer = tnn.Sequential(
+        tnn.Conv2d(chann_in, chann_out, kernel_size=k_size, padding=p_size),
+        tnn.BatchNorm2d(chann_out),
+        tnn.ReLU()
     )
     return layer
 
-class VGG16(nn.Module):
+def vgg_conv_block(in_list, out_list, k_list, p_list, pooling_k, pooling_s):
+
+    layers = [ conv_layer(in_list[i], out_list[i], k_list[i], p_list[i]) for i in range(len(in_list)) ]
+    layers += [ tnn.MaxPool2d(kernel_size = pooling_k, stride = pooling_s)]
+    return tnn.Sequential(*layers)
+
+def vgg_fc_layer(size_in, size_out):
+    layer = tnn.Sequential(
+        tnn.Linear(size_in, size_out),
+        tnn.BatchNorm1d(size_out),
+        tnn.ReLU()
+    )
+    return layer
+
+class VGG16(tnn.Module):
     def __init__(self, n_classes=1000):
         super(VGG16, self).__init__()
 
-        self.vgg = nn.ModuleList([
-            nn.Conv2d(3, 64, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
+        # Conv blocks (BatchNorm + ReLU activation added in each block)
+        self.layer1 = vgg_conv_block([3,64], [64,64], [3,3], [1,1], 2, 2)
+        self.layer2 = vgg_conv_block([64,128], [128,128], [3,3], [1,1], 2, 2)
+        self.layer3 = vgg_conv_block([128,256,256], [256,256,256], [3,3,3], [1,1,1], 2, 2)
+        self.layer4 = vgg_conv_block([256,512,512], [512,512,512], [3,3,3], [1,1,1], 2, 2)
+        self.layer5 = vgg_conv_block([512,512,512], [512,512,512], [3,3,3], [1,1,1], 2, 2)
 
-            nn.Conv2d(64, 128, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-            
-            nn.Conv2d(128, 256, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2, ceil_mode=True),
-            
-            nn.Conv2d(256, 512, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(512, 512, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, 3, 1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, 2),
-
-            nn.Conv2d(512, 1024, 3, 1, padding=6, dilation=6),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(1024, 1024, 1, 1),
-            nn.ReLU(inplace=True),
-        ])
         # FC layers
-        self.layer6 = vgg_fc_layer(20*20*1024, 4096)
+        self.layer6 = vgg_fc_layer(7*7*512, 4096)
         self.layer7 = vgg_fc_layer(4096, 4096)
 
         # Final layer
-        self.layer8 = nn.Linear(4096, n_classes)
+        self.layer8 = tnn.Linear(4096, n_classes)
 
     def forward(self, x):
-        for k in range(len(self.vgg)):
-            out = self.vgg[k](x)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        vgg16_features = self.layer5(out)
+        out = vgg16_features.view(out.size(0), -1)
         out = self.layer6(out)
         out = self.layer7(out)
         out = self.layer8(out)
 
-        return out
+        return vgg16_features, out
 
       
-vgg16 = VGG16(n_classes=100)
+vgg16 = VGG16(n_classes=N_CLASSES)
 vgg16.cuda()
 
-
 # Loss, Optimizer & Scheduler
-cost = nn.CrossEntropyLoss()
+cost = tnn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(vgg16.parameters(), lr=LEARNING_RATE)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
 
@@ -116,7 +94,6 @@ for epoch in range(EPOCH):
 
         # Forward + Backward + Optimize
         optimizer.zero_grad()
-        print(images.shape)
         _, outputs = vgg16(images)
         loss = cost(outputs, labels)
         avg_loss += loss.data
@@ -141,4 +118,4 @@ for images, labels in testLoader:
     print("avg acc: %f" % (100* correct/total))
 
 # Save the Trained Model
-torch.save(vgg16.state_dict(), 'vgg.pth')
+torch.save(vgg16.state_dict(), 'cnn.pkl')
