79718676fd1201c4583b252982ad59399581583b
yuta-0514 <y.akutsu0514@gmail.com>
2022-04-16 19:48:42 +0900

    docker

--- docker.sh
+++ docker.sh
@@ -5,4 +5,4 @@ IMAGES=yuta0514/s3fd
 TAGS=1.8
 PORT=8888
 
-docker run --rm -it --gpus all --ipc host -v ~/dataset:/mnt -v $PWD:/S3FD_pytorch -v ~/.dockerssh:/root/.ssh:ro -p ${PORT}:${PORT} --name ${CONTAINER_NAME} ${IMAGES}:${TAGS}
+docker run --rm -it --gpus all --ipc host -v ~/dataset:/mnt -v $PWD:$PWD -p ${PORT}:${PORT} --name ${CONTAINER_NAME} ${IMAGES}:${TAGS}
